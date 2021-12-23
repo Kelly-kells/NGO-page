a571c765731eb1f5707c99b83fc17fa07beb9579
unknown <kellskelechi@gmail.com>
2021-12-23 14:39:40 +0100

    carousel

--- src/components/pages/Archive.js
+++ src/components/pages/Archive.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { CarouselWrapper } from "react-pretty-carousel"
 
-export const Archive =() => {
+export function Archive() {
     
 
     return (
-        <>
-           <div>
-               <h1>Archive</h1>
-           </div>
-        </>
+        <div className="archive">
+           <CarouselWrapper items={3} mode="gallery">
+              <div className="image image1"></div>
+              <div className="image image-2"></div>
+              <div className="image image-3"></div>
+              <div className="image image1"></div>
+              <div className="image image-2"></div>
+              <div className="image image-3"></div>
+
+           </CarouselWrapper>
+        </div>
     )
 }
+
+export default Archive;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,11 +5,16 @@ export const Home =() => {
 
     return (
         <>
+        <h1 className='heading'>Innocent Charity Foundation</h1>
+
            <div className='home-container'>
                <div className="home-img">
                    <img src="../images/kids.jpg"alt="africa"/>
                </div>
-               
+               <div className="text">
+                
+               </div>
+            
            </div>
         </>
     )
