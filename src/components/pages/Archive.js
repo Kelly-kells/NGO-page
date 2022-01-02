d9b1896f3212d6730fd36bca092756a1b4656f40
unknown <kellskelechi@gmail.com>
2022-01-02 13:21:20 +0100

    archive-sectin

--- src/components/pages/Archive.js
+++ src/components/pages/Archive.js
@@ -5,7 +5,17 @@ export function Archive() {
 
     return (
         <>
-          ARCHIVE  
+        <div className='archive'>
+        <div className="image-container">
+        <img className='slider' src="/imageFolder/smile.jpg" alt="" />
+        <img className='slider' src="/imageFolder/happy.jpg" alt="" />
+        <img className='slider'  src="/imageFolder/kids.jpg" alt="" />
+        <img className='slider' src="/imageFolder/chance.jpg" alt="" />
+        </div>
+        <div className="button-container">
+          
+        </div>
+        </div>
         </>
     )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -26,6 +26,26 @@ export const Home =() => {
                </div>
             
            </div>
+
+           <div className='container'>
+               <h1>OUR GOALS</h1>
+                  <p className='type-out'>
+                      Innocent charity foundation, is a foundation  that caters for the differently pridveleged.<br />
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sit ad rerum reiciendis amet <br />
+                      similique sequi in sunt aperiam dolorum, ea aliquam ullam eum libero vero impedit aliquid nul<br />
+                      la aut, debitis, id cum delectus laudantium ut natus. Illo rerum, eaque ad ratione quibusdam cum<br />
+                       quaerat quam voluptate maxime aut neque.<br />
+                       Innocent charity foundation, is a foundation  that caters for the differently pridveleged.<br />
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sit ad rerum reiciendis amet <br />
+                      similique sequi in sunt aperiam dolorum, ea aliquam ullam eum libero vero impedit aliquid nul<br />
+                      la aut, debitis, id cum delectus laudantium ut natus. Illo rerum, eaque ad ratione quibusdam cum<br />
+                       quaerat quam voluptate maxime aut neque.
+
+
+
+                  </p>
+               
+           </div>
         </>
     )
 }
